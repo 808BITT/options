cd47065234ffb2a9ee65d5ad444ab6d90439789d
808BiTT <bobbitt808@gmail.com>
2024-01-21 22:15:25 -0500

    initial option

---
+++ option.go
@@ -0,0 +1,44 @@
+package option
+
+import (
+	"errors"
+)
+
+var ErrNoValue = errors.New("no value")
+
+type Option[T any] struct {
+	value *T
+}
+
+func Some[T any](value T) Option[T] {
+	return Option[T]{value: &value}
+}
+
+func None[T any]() Option[T] {
+	return Option[T]{}
+}
+
+func (o Option[T]) IsSome() bool {
+	return o.value != nil
+}
+
+func (o Option[T]) IsNone() bool {
+	return o.value == nil
+}
+
+// Get now returns an error if there is no value.
+func (o Option[T]) Get() (T, error) {
+	if o.IsNone() {
+		var zero T
+		return zero, ErrNoValue
+	}
+	return *o.value, nil
+}
+
+// OrElse returns the value if present, otherwise returns the provided default value.
+func (o Option[T]) OrElse(defaultValue T) T {
+	if o.IsSome() {
+		return *o.value
+	}
+	return defaultValue
+}
